8438b5d633a89952454342eac8655a25766f7956
Brijesh Patel <b.patel405@mybvc.ca>
2018-10-29 09:44:26 -0600

    productCOllection class defined

---
+++ POS/POSLibrary/ProductCollection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSLibrary
+{
+    public class ProductCollection
+    {
+        public string Name { get; set; }
+        public List<Product> products;
+
+        public List<Product> GetProducts(string keyWords, string filterType)
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<Product> GetProductsBySearch()
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<Product> GetProductsByCategory()
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<Product> GetProductsByBrands()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
