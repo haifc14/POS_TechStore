70a9fce6167c134600eaf68fcfad3f59b6e8afd9
hai_do_488 <h.do488@mybvc.ca>
2018-10-29 09:35:38 -0600

    Create Product Class

---
+++ POS/POSLibrary/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSLibrary
+{
+    public class Product
+    {
+        public string Name { get; private set; }
+        public int Barcode { get; private set; }
+        public decimal Price { get; private set; }
+        public decimal Discount { get; private set; }
+
+        public Product(string name, int barCode, decimal price, decimal discount)
+        {
+            Name = name;
+            Barcode = barCode;
+            Price = price;
+            Discount = discount;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
