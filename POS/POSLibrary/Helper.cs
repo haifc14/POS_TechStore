5be2f46430c6804adf23b21844020fc54c07c49c
Brijesh Patel <b.patel405@mybvc.ca>
2018-11-09 14:19:20 -0700

    SQL to Classes defined

--- POS/POSLibrary/Helper.cs
+++ POS/POSLibrary/Helper.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Linq;
 
 namespace POSLibrary
 {
@@ -10,7 +12,21 @@ namespace POSLibrary
     {
         public static void ConnectPOSDB()
         {
+            var context = new DataContext(GetConnectionString("199.103.60.77"));
+            var products = context.GetTable<PProduct>();
+            Console.WriteLine(products.Count());
+            Console.ReadKey();
+        }
 
+        static string GetConnectionString(string serverName)
+        {
+            System.Data.SqlClient.SqlConnectionStringBuilder builder =
+                           new System.Data.SqlClient.SqlConnectionStringBuilder();
+            builder["Data Source"] = serverName;
+            builder["Initial Catalog"] = "bpatel405";
+            builder["User Id"] = "bpatel405";
+            builder["Password"] = "6657975543222680";
+            return builder.ConnectionString;
         }
     }
 }
